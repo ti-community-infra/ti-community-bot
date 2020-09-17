a88b925fc4d2d3c3bfd63857d3e29e465aad2aa6
Rustin-Liu <rustin.liu@gmail.com>
2020-09-17 11:15:43 +0800

    chore: remove useless comments

--- test/index.test.ts
+++ test/index.test.ts
@@ -63,12 +63,3 @@ describe("My Probot app", () => {
     nock.enableNetConnect();
   });
 });
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
