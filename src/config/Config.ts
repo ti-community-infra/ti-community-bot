b8f812f7fa6159dd0ba7d45ccee3790327483d91
二手掉包工程师 <rustin.liu@gmail.com>
2020-09-29 16:18:58 +0800

    fix: use probot config (#37)
    * fix: use probot config
    
    * fix: add base const

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -4,6 +4,7 @@ export interface Config {
 }
 
 export const DEFAULT_SIG_INFO_FILE_NAME = "member-list";
+export const DEFAULT_BASE = ".github";
 export const DEFAULT_CONFIG_FILE_PATH = "community-bot.yml";
 export const DEFAULT_SIG_INFO_FILE_EXT = ".json";
 export const MAX_SIG_INFO_FILE_CHANGE_NUMBER = 1;
