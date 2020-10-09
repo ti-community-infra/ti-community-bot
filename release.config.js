0db356141940b3241422dca3da93f852ce8f0882
二手掉包工程师 <rustin.liu@gmail.com>
2020-10-09 15:55:46 +0800

    build: add npm release (#54)
    * build: add npm release
    
    * fix: refine fmt

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/git',
-    '@semantic-release/github'
+    '@semantic-release/github',
+    '@semantic-release/npm'
   ]
 }
