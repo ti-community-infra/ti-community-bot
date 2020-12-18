5fe533d3ce520f9360c539b0c6774d9fef944c08
hi-rustin <rustin.liu@gmail.com>
2020-12-18 11:50:33 +0800

    test: add api cases

--- src/api/contributor/index.ts
+++ src/api/contributor/index.ts
@@ -5,19 +5,26 @@ import { StatusCodes } from "http-status-codes";
 import { Response as Res } from "../../services/response";
 import { ContributorMessage } from "../../services/messages/ContributorMessage";
 
+/**
+ * List contributors.
+ * @param req
+ * @param res
+ * @param contributorService
+ */
 export async function listContributors(
   req: Request,
   res: Response,
   contributorService: IContributorsService
 ) {
-  // Gather params.
+  // Gather paginate query.
   const { current, pageSize } = req.query;
   let paginateQuery;
+
+  // Validate paginate query.
   if (current !== undefined && pageSize !== undefined) {
     const currentNum = Number(current);
     const pageSizeNum = Number(pageSize);
     if (!Number.isInteger(currentNum) || !Number.isInteger(pageSizeNum)) {
-      res.status(StatusCodes.BAD_REQUEST);
       const response: Res<null> = {
         data: null,
         status: StatusCodes.BAD_REQUEST,
---
+++ test/api/contributor/index.test.ts
@@ -0,0 +1,120 @@
+import { StatusCodes } from "http-status-codes";
+import { Request as Req, Response as Res } from "express";
+import typeorm = require("typeorm");
+
+import { Response } from "../../../src/services/response";
+import { ContributorsDTO } from "../../../src/services/dtos/ContributorsDTO";
+import { IContributorsService } from "../../../src/services/contributor";
+import { PaginateQuery } from "../../../src/queries/PaginateQuery";
+import { listContributors } from "../../../src/api/contributor";
+import { ContributorMessage } from "../../../src/services/messages/ContributorMessage";
+
+describe("Contributors API", () => {
+  beforeEach(() => {
+    // Mock the db connection.
+    typeorm.createConnection = jest.fn().mockResolvedValue(null);
+  });
+
+  test("List all contributors", async () => {
+    // No paginate query.
+    const mockRequest = ({
+      query: {},
+    } as unknown) as Req;
+
+    const json = jest.fn();
+    const status = jest.fn();
+    const mockResponse = ({
+      status,
+      json,
+    } as unknown) as Res;
+
+    const response: Response<ContributorsDTO> = {
+      data: {
+        contributors: [
+          {
+            githubName: "contributor1",
+          },
+          {
+            githubName: "contributor2",
+          },
+          {
+            githubName: "contributor3",
+          },
+        ],
+        total: 3,
+      },
+      status: StatusCodes.OK,
+      message: "Test",
+    };
+
+    const mockContributorService: IContributorsService = {
+      listContributors(_?: PaginateQuery): Promise<Response<ContributorsDTO>> {
+        return Promise.resolve(response);
+      },
+    };
+
+    // Get a sig.
+    await listContributors(mockRequest, mockResponse, mockContributorService);
+
+    // Assert response.
+    expect(json.mock.calls.length).toBe(1);
+    expect(json.mock.calls[0][0]).toBe(response);
+    // Assert status.
+    expect(status.mock.calls.length).toBe(1);
+    expect(status.mock.calls[0][0]).toBe(StatusCodes.OK);
+  });
+
+  test("List all contributors with illegal paginate query", async () => {
+    // No paginate query.
+    const mockRequest = ({
+      query: {
+        current: "invalid",
+        pageSize: "invalid",
+      },
+    } as unknown) as Req;
+
+    const json = jest.fn();
+    const status = jest.fn();
+    const mockResponse = ({
+      status,
+      json,
+    } as unknown) as Res;
+
+    const response: Response<ContributorsDTO> = {
+      data: {
+        contributors: [
+          {
+            githubName: "contributor1",
+          },
+          {
+            githubName: "contributor2",
+          },
+          {
+            githubName: "contributor3",
+          },
+        ],
+        total: 3,
+      },
+      status: StatusCodes.OK,
+      message: "Test",
+    };
+
+    const mockContributorService: IContributorsService = {
+      listContributors(_?: PaginateQuery): Promise<Response<ContributorsDTO>> {
+        return Promise.resolve(response);
+      },
+    };
+
+    // Get a sig.
+    await listContributors(mockRequest, mockResponse, mockContributorService);
+
+    // Assert response.
+    expect(json.mock.calls.length).toBe(1);
+    expect(json.mock.calls[0][0].message).toBe(
+      ContributorMessage.IllegalQueryParameters
+    );
+    // Assert status.
+    expect(status.mock.calls.length).toBe(1);
+    expect(status.mock.calls[0][0]).toBe(StatusCodes.BAD_REQUEST);
+  });
+});
