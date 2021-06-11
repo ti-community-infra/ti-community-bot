9c3c86fe62b8af1585550994f8822bdfe813b986
hi-rustin <rustin.liu@gmail.com>
2021-06-11 15:15:02 +0800

    fix: revert semantic-release/git opts

--- release.config.js
+++ release.config.js
@@ -4,14 +4,7 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "package-lock.json"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/git",
     "@semantic-release/github",
   ],
 };
