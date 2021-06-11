ce002a2312eb058aeaa9bee176340c8ff5c0b509
二手掉包工程师 <rustin.liu@gmail.com>
2021-06-11 15:38:16 +0800

    fix: don't skip the publish ci (#375)

--- release.config.js
+++ release.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json"],
+        message:
+          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
+      },
+    ],
     "@semantic-release/github",
   ],
 };
